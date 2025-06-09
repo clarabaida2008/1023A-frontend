723751f222cf8f2ea2eea470920e90ef68334cb8
clarabaida2008 <csplicidobaida@gmail.com>
2025-06-09 09:52:14 -0400

    Aula 09/06

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,6 +1,11 @@
-function Header() {
-  return (
-    <h1>Header</h1>
-  )
-}
+//States
+//Props --> Informações que você pode passar para um componente
+//Hooks
+
+import react from 'react';
+class Header extends react.Component {
+  render(): react.ReactNode {
+    return <h1>Header</h1>
+  }
+}                                                                                             
 export default Header
